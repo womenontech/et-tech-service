d8ce14a02167ae4dbf1efb38f25cb086c4522bdf
unknown <womenontech@gmail.com>
2023-03-22 13:56:41 +0800

    [BASECODE]

---
+++ src/main/java/com/example/ettechservice/EtTechServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ettechservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtTechServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtTechServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ettechservice/EtTechServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ettechservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtTechServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
